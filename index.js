c7caa253cc1f83824f9e68f88c6daa82cfcd7af4
sou1burn <koryakinv_04@mail.ru>
2024-03-25 21:21:41 +0300

    3 laba dodelka

--- index.js
+++ index.js
@@ -1,46 +1,11 @@
-setInterval(ph_data, 1000)
+/*setInterval(ph_data, 1000)
 setInterval(fridge_data, 1000)
 setInterval(cfm_data, 1000)
 setInterval(sys_data, 1000)
 setInterval(lf_data, 1000)
+*/
 
-function ph_data(){
-$.ajax({
-type: 'GET',
-url: '/connect_ph',
-dataType: 'json',
-contentType: 'application/json',
-data: {},
-success: function(response){
-document.getElementById("sleep_time").value = response["sleep_time"]}
-});
-}
-
-function fridge_data(){
-$.ajax({
-type: 'GET',
-url: '/connect_fridge',
-dataType: 'json',
-contentType: 'application/json',
-data: {},
-success: function(response){
-document.getElementById("value").value = response["value"]}
-});
-}
-
-function cfm_data(){
-$.ajax({
-type: 'GET',
-url: '/connect_cfm',
-dataType: 'json',
-contentType: 'application/json',
-data: {},
-success: function(response){
-document.getElementById("value1").value = response["value"]}
-});
-}
-
-function sys_data(){
+function submitPassword(){
 $.ajax({
 type: 'GET',
 url: '/connect_sys',
@@ -52,7 +17,7 @@ document.getElementById("password_state").value = response["password"]}
 });
 }
 
-function lf_data(){
+function submitTempState(){
 $.ajax({
 type: 'GET',
 url: '/connect_lf',
@@ -63,3 +28,47 @@ success: function(response){
 document.getElementById("temp_state").value = response["temp"]}
 });
 }
+
+function submitSleepTime(){
+$.ajax({
+type: 'GET',
+url: '/connect_ph',
+dataType: 'json',
+contentType: 'application/json',
+data: {},
+success: function(response){
+document.getElementById("sleep_time").value = response["sleep_time"]}
+});
+}
+
+function submitFridgeValue(){
+$.ajax({
+type: 'GET',
+url: '/connect_fridge',
+dataType: 'json',
+contentType: 'application/json',
+data: {},
+success: function(response){
+document.getElementById("value").value = response["value"]}
+});
+}
+
+function submitCoffeemachineValue(){
+$.ajax({
+type: 'GET',
+url: '/connect_cfm',
+dataType: 'json',
+contentType: 'application/json',
+data: {},
+success: function(response){
+document.getElementById("value1").value = response["value"]}
+});
+}
+
+
+
+/*document.getElementById("submitSleepTime()").addEventListener("click", ph_data);
+document.getElementById("submitFridgeValue()").addEventListener("click", get_fridge_data);
+document.getElementById("submitCoffeemachineValue()").addEventListener("click", get_cfm_data);
+document.getElementById("submitPassword()").addEventListener("click", get_sys_data);
+document.getElementById("submitTempState()").addEventListener("click", get_lf_data);*/
